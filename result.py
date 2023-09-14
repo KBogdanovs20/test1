9c7487be5920aad1f37cecabda0a281b51dbfa09
KBogdanovs20 <144994793+KBogdanovs20@users.noreply.github.com>
2023-09-14 11:41:25 +0300

    Create result.py
    14.09.2023.

---
+++ result.py
@@ -0,0 +1,17 @@
+from openpyxl import Workbook, load_workbook 
+wb=load_workbook('tests/test1.xlsx')
+ws=wb.active
+total=0
+#write your code here
+max_row = ws.max_row
+max_salary = 3000
+for row in range(2,max_row+1):
+    hours = ws['B'+str(row)].value
+    rate = ws['C'+str(row)].value
+    if (type(hours)!=str and type(rate)!=str):
+        salary=float(hours)*float(rate)
+        ws['D'+str(row)].value=salary
+        if salary > max_salary:
+            total += 1
+print(total)
+wb.close()
